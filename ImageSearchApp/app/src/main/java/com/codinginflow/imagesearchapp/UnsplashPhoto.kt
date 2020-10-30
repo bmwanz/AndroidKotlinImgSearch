fddf574f578594dbe683283c122cf48e0ff6cfb1
BW <bmwanz@live.com>
2020-10-30 14:07:01 -0700

    Part 2 - Layout & Model

---
+++ ImageSearchApp/app/src/main/java/com/codinginflow/imagesearchapp/UnsplashPhoto.kt
@@ -0,0 +1,35 @@
+package com.codinginflow.imagesearchapp
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+/*
+    documentation:
+    https://unsplash.com/documentation#search-photos
+ */
+
+@Parcelize
+data class UnsplashPhoto(
+    val id: String,
+    val description: String?,
+    val urls: UnsplashPhotoUrls,
+    val user: UnsplashUser
+) : Parcelable {
+
+    @Parcelize
+    data class UnsplashPhotoUrls(
+        val raw: String,
+        val full: String,
+        val regular: String,
+        val small: String,
+        val thumb: String
+    ) : Parcelable
+
+    @Parcelize
+    data class UnsplashUser(
+        val name: String,
+        val username: String
+    ) : Parcelable {
+        val attributionUrl get() = "https://unsplash.com/$username?utm_source=AndroidKotlinImgSearch&utm_medium=referral"
+    }
+}
